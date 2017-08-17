da9c09211c4fce7b5fd8572469dde64ce167ba8c
J3ffcon1 <Jrlone1990@gmail.com>
2017-08-16 20:24:58 -0700

    added functions to salmon_cookie

--- salmon_cookie.js
+++ salmon_cookie.js
@@ -2,15 +2,37 @@ var PioneerSquare = {
 minCustomerHr: 17,
 maxCustomerHr: 88,
 avgCustomerHr: 5.2,
+storeCookies: [],
+storeHours: ["10am", "11am", "12pm","1pm", "2pm", "3pm", "4pm", "5pm"],
 randomNumber: function() {
   return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr + 1 )) + this.minCustomerHr;
+},
+
+randomCookies: function(){
+  return (this.randomNumber() * this.avgCustomerHr);
+},
+
+numberCookies: function () {
+  for (var i = 0; i < this.storeHours.length; i++) {
+    this.storeCookies.push(this.randomCookies());
+  }
+},
+displayCookies: function(){
+  for (var i = 0; i < Pi.length; i++) {
+    Pi[i]
+  }
+
 }
+
+
+
 }
 
 var PortlandAirport = {
 minCustomerHr: 6,
 maxCustomerHr: 24,
 avgCustomerHr: 1.2,
+storeHours: ["10am", "11am", "12pm","1pm", "2pm", "3pm", "4pm", "5pm"],
 randomNumber: function() {
   return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr + 1 )) + this.minCustomerHr;
 }
@@ -20,6 +42,7 @@ var WashingtonSquare = {
 minCustomerHr: 11,
 maxCustomerHr: 38,
 avgCustomerHr: 1.9,
+storeHours: ["10am", "11am", "12pm","1pm", "2pm", "3pm", "4pm", "5pm"],
 randomNumber: function() {
   return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr + 1 )) + this.minCustomerHr;
 }
@@ -29,6 +52,7 @@ var Sellwood = {
 minCustomerHr: 20,
 maxCustomerHr: 48,
 avgCustomerHr: 3.3,
+storeHours: ["10am", "11am", "12pm","1pm", "2pm", "3pm", "4pm", "5pm"],
 randomNumber: function() {
   return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr + 1 )) + this.minCustomerHr;
 }
@@ -38,6 +62,7 @@ var PearlDistrict = {
 minCustomerHr: 3,
 maxCustomerHr: 24,
 avgCustomerHr: 2.6,
+storeHours: ["10am", "11am", "12pm","1pm", "2pm", "3pm", "4pm", "5pm"],
 randomNumber: function() {
   return Math.floor(Math.random() * (this.maxCustomerHr - this.minCustomerHr + 1 )) + this.minCustomerHr;
 }
